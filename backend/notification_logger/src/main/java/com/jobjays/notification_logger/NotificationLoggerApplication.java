cc02c1fa2a9684dd9755e860c0c96f37bc972f54
muradazimzada <murad.azimzada.dev@gmail.com>
2024-10-14 19:24:49 -0400

    changed the directory of the NotLogApp.java to its root dir

--- backend/notification_logger/src/main/java/com/jobjays/notification_logger/NotificationLoggerApplication.java
+++ backend/notification_logger/src/main/java/com/jobjays/notification_logger/NotificationLoggerApplication.java
@@ -1,4 +1,4 @@
-package com.example.notificationlogger;
+package com.jobjays.notification_logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
