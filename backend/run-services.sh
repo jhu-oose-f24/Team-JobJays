dd8c330fbf80104d1ba2dbb2674bdb4d0566baed
muradazimzada <murad.azimzada.dev@gmail.com>
2024-10-16 15:48:56 -0400

    Updated running scripts

--- backend/run-services.sh
+++ backend/run-services.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+
+# Run Docker Compose to start all services
+echo "Starting services with Docker Compose..."
+docker-compose up --build -d
 # Navigate to each service and run gradle build
 SERVICES=("applicant_job_matcher" "notification_logger" "notification_sender" "preference_updater")
 
 for SERVICE in "${SERVICES[@]}"; do
   echo "Building $SERVICE..."
   cd "$SERVICE" || exit
-  ./gradlew build
+  ./gradlew build -x test
   if [ $? -ne 0 ]; then
     echo "Failed to build $SERVICE"
     exit 1
@@ -14,6 +18,3 @@ for SERVICE in "${SERVICES[@]}"; do
   cd ..
 done
 
-# Run Docker Compose to start all services
-echo "Starting services with Docker Compose..."
-docker-compose up --build
